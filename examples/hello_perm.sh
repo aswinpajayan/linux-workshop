99581a6c2ebfc9b83a5b5401721b961a87f25bb9
Aswin Ajayan <aswinp2008@gmail.com>
2019-07-24 03:24:46 +0530

    added permision example

---
+++ examples/hello_perm.sh
@@ -0,0 +1,8 @@
+echo "   "
+echo -e  "\033[1;33mYayy you unlocked me\033[0m" >&2
+echo "   "
+echo "   "
+echo "   "
+echo "   "
+echo -e  "\033[1;33mNow you try to read me\033[0m" >&2
+#of course you are an iitian , i knew you could open me
